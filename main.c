1d136f8bc4b403a8b72527e2051cb67502d3b5f2
CamilaUrquizo <camilaurquizo13@gmail.com>
2024-12-11 14:21:01 -0500

    main.c

--- main.c
+++ main.c
@@ -1,39 +1,52 @@
+
 #include <stdio.h>
+#include <string.h>
 #include "funciones.h"
 
-int main (int argc, char *argv[]) {
+int main() {
+    struct Libro libros[20]; 
+    int i = 0; 
+    int opcion; 
 
-    struct Libro libros[20];
-    int opc;
-    int i=0;
-    do{
-    opc = menu();
-    switch (opc)
-    {
-        case 1:
-            registrarLibros(libros, i);
-            break;
-        case 2:
-            mostrarLibros(libros);
-            break;
-        case 3:
-            int id;
-            printf("Ingrese el id del libro a buscar: ");
-            scanf("%d", &id);
-            buscarLibroId(libros, id);
-            i++;
-            break;
-        case 4:
-            char titulo[100];
-            printf("Ingrese el titulo del libro a buscar: ");
-            scanf("%s", titulo);
-            buscarLibroTitulo(libros, titulo);
-            break;
-        
+    do {
+        printf("\nMENU\n");
+        printf("1. Registrar libro\n");
+        printf("2. Mostrar libros\n");
+        printf("3. Buscar libro por ID\n");
+        printf("4. Buscar libro por titulo\n");
+        printf("5. Actualizar estado de libro\n");
+        printf("6. Eliminar libro\n");
+        printf("7. Salir\n");
+        printf("Seleccione una opcion: ");
+        scanf("%d", &opcion);
 
-    }
-    }while(opc != 7);
-
+        while (getchar() != '\n'); 
+        switch(opcion) {
+            case 1:
+                registrarLibro(libros, &i);
+                break;
+            case 2:
+                mostrarLibrosAntesAccion(libros, i);
+                break;
+            case 3:
+                buscarLibroId(libros, i);
+                break;
+            case 4:
+                buscarLibroTitulo(libros, i);
+                break;
+            case 5:
+                actualizarEstado(libros, i);
+                break;
+            case 6:
+                eliminarLibro(libros, &i);
+                break;
+            case 7:
+                printf("Saliendo del programa...\n");
+                break;
+            default:
+                printf("Opcion no valida. Intente de nuevo.\n");
+        }
+    } while(opcion != 7);
 
     return 0;
 }
